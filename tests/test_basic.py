0f561496033895cbf8e44b7c2fea451a51f41f89
bedbad <antonyuk@bu.edu>
2024-09-03 17:53:30 +0300

    basic usage test from readme

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,23 +2,19 @@
 # matplotlib.use('Agg')
 # import matplotlib.pyplot as plt
 import numpy as np 
-import time
 import cv2
-
+import time
 from justpyplot import justpyplot as jplt
 
-t0 = time.perf_counter()
-
+t0=time.perf_counter()
 xs, ys = [], []
-c = 0
-while(c != 27):
+while(cv2.waitKey(1) != 27):
     xt = time.perf_counter() - t0
     yx = np.sin(xt)
     xs.append(xt)
     ys.append(yx)
-    frame = np.full((400,400,3), (255,255,255), dtype=np.uint8)
-    vals = np.array([xs,ys])
-    final = jplt.plot2_at(img_array=frame,values=vals,offset=(50,50),title="sin() from Clock")
-
-    cv2.imshow('frame', final)
-    c = cv2.waitKey(1)
+    
+    vals = np.array(ys)
+    plotted_array = jplt.plot1(vals,title="sin() from Clock")
+    
+    cv2.imshow('np array plot', plotted_array)
