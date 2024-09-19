e90c16544c290a1c0acc5aaf96bbdfed6f5f88a5
Esaul Parra Garcia <epg323@ucla.edu>
2024-09-18 21:52:02 -0700

    format

--- examples/mug_objectron/demo.py
+++ examples/mug_objectron/demo.py
@@ -7,7 +7,7 @@ from scipy.spatial.transform import Rotation as R
 from justpyplot import justpyplot as jplt
 
 
-#Most popular - Macbook Air "13, in centimenters
+# Most popular - Macbook Air "13, in centimenters
 # px_sz = 0.01119
 # monitor= screeninfo.get_monitors()[0]
 # screen_width, screen_height = monitor.width, monitor.height
@@ -16,25 +16,29 @@ from justpyplot import justpyplot as jplt
 # px_sz = screen_width_phys / screen_width
 # print("Pixel Size on system: {}, cm".format(px_sz*.1))
 
-def reject_outliers(data, max_len=10, m =2):
+
+def reject_outliers(data, max_len=10, m=2):
     if data.size < 3:
         return data
 
-    tail, last_data = data[:-max_len],data[-max_len:]
+    tail, last_data = data[:-max_len], data[-max_len:]
     u = np.mean(data)
     s = np.std(data)
     filtered_data = last_data[abs(last_data - u) < s]
-    data=np.concatenate([tail, filtered_data])
+    data = np.concatenate([tail, filtered_data])
     return data
+
 
 def main():
     # Initialize MediaPipe Objectron
     mp_objectron = mp.solutions.objectron
-    objectron = mp_objectron.Objectron(static_image_mode=False,
-                                        max_num_objects=1,
-                                        min_detection_confidence=0.4,
-                                        min_tracking_confidence=0.25,
-                                        model_name='Cup')
+    objectron = mp_objectron.Objectron(
+        static_image_mode=False,
+        max_num_objects=1,
+        min_detection_confidence=0.4,
+        min_tracking_confidence=0.25,
+        model_name='Cup',
+    )
 
     # Initialize MediaPipe Drawing Utils
     mp_drawing = mp.solutions.drawing_utils
@@ -51,7 +55,7 @@ def main():
     while cap.isOpened():
         success, image = cap.read()
         if not success:
-            print("Ignoring empty camera frame.")
+            print('Ignoring empty camera frame.')
             continue
 
         # Convert the BGR image to RGB
@@ -78,30 +82,54 @@ def main():
                 angles_x.append(rotations[2])
                 volumes.append(volume_size)
                 distances.append(distance)
-                ang_zs = (np.array(angles_z))
-                ang_ys = (np.array(angles_y))
-                ang_xs = (np.array(angles_x))
+                ang_zs = np.array(angles_z)
+                ang_ys = np.array(angles_y)
+                ang_xs = np.array(angles_x)
 
-                jplt.plot1_at(image, ang_zs,
-                            title='Angle from Z axis', offset=(50,50), size=(270, 300),
-                            point_color=(255,0,0),line_color=(255,0,0), label_color=(255,0,0), grid_color=(126,126,126))
-                jplt.plot1_at(image, ang_ys,
-                              title='Angle from Y axis', offset=(400,50), size=(270, 300),
-                              point_color=(0,255,0), line_color=(0,255,0),label_color=(0,255,0), grid_color=(126,126,126),
-                              scatter=False)
-                jplt.plot1_at(image,ang_xs,
-                              title='Angle from X axis', offset=(750,50), size=(270, 300),
-                              point_color=(0,0,255), line_color=(0,0,255),label_color=(0,0,255), grid_color=(126,126,126),
-                              scatter=False)
-               
-
+                jplt.plot1_at(
+                    image,
+                    ang_zs,
+                    title='Angle from Z axis',
+                    offset=(50, 50),
+                    size=(270, 300),
+                    point_color=(255, 0, 0),
+                    line_color=(255, 0, 0),
+                    label_color=(255, 0, 0),
+                    grid_color=(126, 126, 126),
+                )
+                jplt.plot1_at(
+                    image,
+                    ang_ys,
+                    title='Angle from Y axis',
+                    offset=(400, 50),
+                    size=(270, 300),
+                    point_color=(0, 255, 0),
+                    line_color=(0, 255, 0),
+                    label_color=(0, 255, 0),
+                    grid_color=(126, 126, 126),
+                    scatter=False,
+                )
+                jplt.plot1_at(
+                    image,
+                    ang_xs,
+                    title='Angle from X axis',
+                    offset=(750, 50),
+                    size=(270, 300),
+                    point_color=(0, 0, 255),
+                    line_color=(0, 0, 255),
+                    label_color=(0, 0, 255),
+                    grid_color=(126, 126, 126),
+                    scatter=False,
+                )
 
                 # print("Rotation:\n", rotations)
                 # print("Volume Size:", volume_size)
                 # print("3D Distance:", distance)
 
                 # Draw the 3D bounding box and axis
-                mp_drawing.draw_landmarks(image, detected_object.landmarks_2d, mp_objectron.BOX_CONNECTIONS)
+                mp_drawing.draw_landmarks(
+                    image, detected_object.landmarks_2d, mp_objectron.BOX_CONNECTIONS
+                )
                 mp_drawing.draw_axis(image, rotation, translation)
 
         # Display the annotated image
@@ -118,5 +146,6 @@ def main():
     cap.release()
     cv2.destroyAllWindows()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
